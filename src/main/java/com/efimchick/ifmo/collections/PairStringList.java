7c5e32d4ddcf8b693d65b4441a46fa6ac3230285
Pavel Naumovets <pavel.naumovets@mail.ru>
2021-06-24 07:33:50 +0300

    Answer

--- src/main/java/com/efimchick/ifmo/collections/PairStringList.java
+++ src/main/java/com/efimchick/ifmo/collections/PairStringList.java
@@ -1,18 +1,66 @@
 package com.efimchick.ifmo.collections;
 
-import java.sql.SQLOutput;
 import java.util.*;
 
-class PairStringList  {
+class PairStringList implements List<String>{
     public List<String> list = new ArrayList<>();
 
-    public void add(String param){
-        for (int i = 0; i < 2; i++) {
-            list.add(param);
-        }
+
+    @Override
+    public String toString() {
+        return String.valueOf(list);
+    }
+    //Сделал
+    @Override
+    public int size() {
+        return list.size();
     }
 
-    public void add(int index, String param){
+    @Override
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    @Override
+    public Iterator<String> iterator() {
+        return new Iterator<String>() {
+            private int currentIndex = 0;
+            @Override
+            public boolean hasNext() {
+                return currentIndex < list.size();
+            }
+
+            @Override
+            public String next() {
+                if(currentIndex == list.size()){
+                    throw new NoSuchElementException();
+                }
+                currentIndex++;
+                return list.get(currentIndex - 1);
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(String param) {
+        for (int i = 0; i < 2; i++) {
+               list.add(param);
+        }
+        return true;
+    }
+
+    @Override
+    public void add(int index, String param) {
         if(index % 2 != 0){
             for (int i = 0; i < 2; i++) {
                 list.add(index + 1, param);
@@ -24,13 +72,16 @@ class PairStringList  {
         }
     }
 
-    public void remove(String param){
+    @Override
+    public boolean remove(Object param) {
         for (int i = 0; i < 2; i++) {
             list.remove(param);
         }
+        return false;
     }
 
-    public void remove(int index){
+    @Override
+    public String remove(int index) {
         if(index % 2 != 0){
             for (int i = 0; i < 2; i++, index--) {
                 list.remove(index);
@@ -40,13 +91,16 @@ class PairStringList  {
                 list.remove(index);
             }
         }
+        return null;
     }
 
-    public void get(int index){
-        list.get(index);
+    @Override
+    public String get(int index) {
+        return list.get(index);
     }
 
-    public void set(int index, String param){
+    @Override
+    public String set(int index, String param) {
         if(index % 2 != 0){
             for (int i = 0; i < 2; i++, index--) {
                 list.set(index, param);
@@ -56,38 +110,153 @@ class PairStringList  {
                 list.set(index, param);
             }
         }
+        return null;
     }
 
-    public int size(){
-        return list.size();
-    }
-
-    public void addAll(Collection<? extends String> list){
+    @Override
+    public boolean addAll(Collection<? extends String> list) {
         for(String l : list){
             add(l);
         }
+        return true;
     }
 
-    public void addAll(int index, Collection<? extends String> list){
+    @Override
+    public boolean addAll(int index, Collection<? extends String> list) {
         ArrayList<String> arr = new ArrayList<>(list);
         for (int i = arr.size() - 1; i >= 0 ; i--) {
             add(index, arr.get(i));
         }
-    }
-
-    public void clear(){
-        list.clear();
-    }
-
-    public void iterator(){
-        Iterator<String> iterator = list.iterator();
-        while(iterator.hasNext()){
-            iterator.next();
-        }
+        return true;
     }
 
     @Override
-    public String toString() {
-        return String.valueOf(list);
+    public void clear() {
+        list.clear();
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
     }
 }
+
+//    public void add(String param){
+//        for (int i = 0; i < 2; i++) {
+//            list.add(param);
+//        }
+//    }
+//
+//    public void add(int index, String param){
+//        if(index % 2 != 0){
+//            for (int i = 0; i < 2; i++) {
+//                list.add(index + 1, param);
+//            }
+//        } else{
+//            for (int i = 0; i < 2; i++) {
+//                list.add(index, param);
+//            }
+//        }
+//    }
+//
+//    public void remove(String param){
+//        for (int i = 0; i < 2; i++) {
+//            list.remove(param);
+//        }
+//    }
+//
+//    public void remove(int index){
+//        if(index % 2 != 0){
+//            for (int i = 0; i < 2; i++, index--) {
+//                list.remove(index);
+//            }
+//        } else{
+//            for (int i = 0; i < 2; i++) {
+//                list.remove(index);
+//            }
+//        }
+//    }
+//
+//    public void get(int index){
+//        list.get(index);
+//    }
+//
+//    public void set(int index, String param){
+//        if(index % 2 != 0){
+//            for (int i = 0; i < 2; i++, index--) {
+//                list.set(index, param);
+//            }
+//        } else{
+//            for (int i = 0; i < 2; i++, index++) {
+//                list.set(index, param);
+//            }
+//        }
+//    }
+//
+//    public int size(){
+//        return list.size();
+//    }
+//
+//    public void addAll(Collection<? extends String> list){
+//        for(String l : list){
+//            add(l);
+//        }
+//    }
+//
+//    public void addAll(int index, Collection<? extends String> list){
+//        ArrayList<String> arr = new ArrayList<>(list);
+//        for (int i = arr.size() - 1; i >= 0 ; i--) {
+//            add(index, arr.get(i));
+//        }
+//    }
+//
+//    public void clear(){
+//        list.clear();
+//    }
+//
+//    public void iterator(){
+//        Iterator<String> iterator = list.iterator();
+//        while(iterator.hasNext()){
+//            iterator.next();
+//        }
+//    }
--- src/main/java/com/efimchick/ifmo/collections/SortedByAbsoluteValueIntegerSet.java
+++ src/main/java/com/efimchick/ifmo/collections/SortedByAbsoluteValueIntegerSet.java
@@ -1,5 +1,106 @@
 package com.efimchick.ifmo.collections;
 
-class SortedByAbsoluteValueIntegerSet {
+import java.util.*;
 
+class SortedByAbsoluteValueIntegerSet implements Set<Integer> {
+    Set<Integer> set = new HashSet<>();
+
+//    public static void main(String[] args) {
+//        SortedByAbsoluteValueIntegerSet arr =new SortedByAbsoluteValueIntegerSet();
+//        arr.addAll(Arrays.asList(1, 3, 5, 7, 9));
+//        System.out.println(arr);
+//        arr.addAll(Arrays.asList(-2, -4, -6, -8, -10));
+//        System.out.println(arr);
+//
+//    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(set);
+    }
+
+    @Override
+    public int size() {
+        return set.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return set.size() == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return set.contains(o);
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            private ArrayList<Integer> arr = new ArrayList<>(set);
+            private int currentIndex = 0;
+            @Override
+            public boolean hasNext() {
+                return currentIndex < arr.size();
+            }
+
+            @Override
+            public Integer next() {
+                if(currentIndex == arr.size()){
+                    throw new NoSuchElementException();
+                }
+
+                currentIndex++;
+                return arr.get(currentIndex - 1);
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Integer param) {
+        return set.add(param);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return set.remove(o);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Integer> set) {
+        for(Integer i : set){
+            this.set.add(i);
+        }
+        return true;
+    }
+
+    @Override
+    public void clear() {
+        set.clear();
+    }
+
+    @Override
+    public boolean removeAll(Collection param) {
+        return set.removeAll(param);
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
 }
